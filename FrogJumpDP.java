f7bb3182a4c10de513463d725b63b34ae6c3f9b8
RatneshChouhan <60027002+RatneshChouhan@users.noreply.github.com>
2022-03-20 09:05:12 +0530

    Version-2 - Removed tabulation [Bottom-Up Approach] and improved with space optimization, Removed DP array

--- FrogJumpDP.java
+++ FrogJumpDP.java
@@ -1,8 +1,6 @@
-import java.util.Arrays;
-
 public class FrogJumpDP {
-    // DP Solution
-    /*static int f(int n, int [] h, int [] dp) {
+    /* DP Solution Time Complexity - O(N) , Space Complexity - O(N) -> due to recursion stack
+    static int f(int n, int [] h, int [] dp) {
         if(dp[n] != -1) return dp[n];
         if(n==0) return 0;
         int l = f(n-1, h, dp) + Math.abs(h[n] - h[n-1]);
@@ -10,9 +8,10 @@ public class FrogJumpDP {
         if(n > 1)
             r = f(n-2, h, dp) + Math.abs(h[n] - h[n-2]);
         return dp[n] = Math.min(l,r);
-    }*/
+    }
+    */
 
-    /*  Tabulation Solution */
+    /*  Tabulation Solution Time Complexity - O(N) , Space Complexity - O(N) -> No Recursion But Extra dp array
     static int frogJump(int n, int[] heights) {
         // Write your code here..
          int [] dp = new int[n];
@@ -27,6 +26,25 @@ public class FrogJumpDP {
         }
         return dp[n-1] ;
     }
+    */
+
+    /* Space Optimized Method Time Complexity - O(N) , Space Complexity - O(1) -> No Recursion, No Extra Array */
+    static int frogJump(int n, int[] heights) {
+        int prev = 0;
+        int prev2 = 0;
+        int r = Integer.MAX_VALUE;
+        for(int i = 1; i < n; i++){
+            int l = prev + Math.abs(heights[i] - heights[i-1]);
+            if(i > 1)
+                r = prev2 + Math.abs(heights[i] - heights[i-2]);
+            int curr = Math.min(l, r);
+            prev2 = prev;
+            prev = curr;
+
+        }
+        return prev ;
+    }
+
 
     public static void main(String[] args) {
         int [] heights = new int [] {10,20,30,10};
